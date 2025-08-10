b79bdb2a0e75d6f4933bd4ce3a909a64c1b863e8
eduard sotelo <ejsotelo4@gmail.com>
2025-08-10 11:11:43 -0500

    actualización en codigo de la infra y cambio del nombre de secret manager

--- infra/envs/dev/variables.tf
+++ infra/envs/dev/variables.tf
@@ -21,7 +21,3 @@ variable "memory" {
   type    = string
   default = "512"
 }
-variable "alb_sg_id" {
-  description = "Security group ID of the Application Load Balancer"
-  type        = string
-}
--- infra/modules/secrets/main.tf
+++ infra/modules/secrets/main.tf
@@ -1,7 +1,11 @@
-resource "aws_secretsmanager_secret" "db" {
-  name = "${var.name}-db-credentials-example"
+resource "random_id" "secret_name" {
+  byte_length = 4  # Ajusta la longitud según lo que necesites
 }
+resource "aws_secretsmanager_secret" "db" {
+  name = "${var.name}-db-credentials-${random_id.secret_name.hex}"  # Agrega la cadena aleatoria al nombre
 
+  description = "DB credentials stored in Secrets Manager"
+}
 resource "aws_secretsmanager_secret_version" "db_v" {
   secret_id     = aws_secretsmanager_secret.db.id
   secret_string = jsonencode({
